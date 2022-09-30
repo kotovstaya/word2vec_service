514e63d5792cc88cc799035079a86c1993f8316c
Anton <asav@Antons-MacBook-Pro.local>
2022-09-30 18:04:22 +0300

    [TEST-06] create simple service. return topn=10

--- service/service.py
+++ service/service.py
@@ -1,8 +1,19 @@
+import os
 from fastapi import FastAPI
+from gensim.models.word2vec import Word2Vec
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "hello"}
+
+
+@app.get("/entities/{entity}")
+async def get_top_n(entity):
+    root_folder = "./../data/"
+    model_fpath = os.path.join(root_folder, "word2vec_model")
+    model = Word2Vec.load(model_fpath)
+    similarity = model.wv.most_similar(entity, topn=10)
+    return {"message": similarity}
