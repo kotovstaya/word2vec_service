d6597aedeead6ca36d07d5b763c1062177e7fbef
Anton <asav@192.168.1.124>
2022-10-13 22:59:15 +0300

    [TEST-10] docstring for train_base.py added

--- jb/train_base.py
+++ jb/train_base.py
@@ -3,9 +3,15 @@ from preprocessing import get_logger
 
 class BaseTrainer:
     """
-
+    Base class for a model training.
+    Args:
+        :params epochs: how long will it take to train one model
+    Methods:
+        _init_model  - how we should initialize our model
+        train_model - implement this method in children class if you want train a model
+        save_model - how and where should we save our model
     """
-    def __init__(self, epochs):
+    def __init__(self, epochs: int):
         self.epochs = epochs
         self.logger = get_logger(BaseTrainer.__name__)
 
