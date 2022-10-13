62d33b7ebe1e25f22a2e7533271213b886c82789
Anton <asav@192.168.1.124>
2022-10-13 22:56:09 +0300

    [TEST-10] dicstring for proprocessing.py added

--- jb/preprocessing.py
+++ jb/preprocessing.py
@@ -20,38 +20,50 @@ def get_logger(name):
 
 
 def preprocess_corpus(path: str,
-                      limit: tp.Optional[int] = None
+                      limit: tp.Optional[int] = None,
+                      step: int = 200,
+                      min_value: int = 20,
                       ) -> tp.List[tp.List[str]]:
     """
+    Create a corpus of proper size.
+    Sometimes it happens that line of some file is very long.
+    So we need to split a long line into chunks of the specific size
 
-    :param path:
-    :param limit:
-    :return:
+    Args
+        :param path: file where each line is a .py file with code
+        :param limit: if you test something and don't want to get all lines
+        :param step: valid size for string's split
+        :param min_value: if length of line is less than this value than skip this line
+    :return: List of prepared list of words
     """
+    logger = get_logger("preprocess_corpus")
     corpus = []
-    step = 200
+    step_width = 100
     with open(path, "r") as f:
         for ix, line in tqdm.tqdm(enumerate(f.readlines())):
             if limit is not None and ix == limit:
                 break
             ll = gensim.utils.simple_preprocess(line)
-            if 20 < len(ll) < step+100:
-                if len(set(ll)) > 10:
+            if min_value < len(ll) < step + step_width:
+                if len(set(ll)) > int(min_value//2):
                     corpus.append(ll)
-            elif len(ll) > step+100:
+            elif len(ll) > step + step_width:
                 for el in range(0, len(ll), step):
                     tmp_line = ll[el: el+step]
-                    if len(tmp_line) > 20 and len(set(tmp_line)) > 10:
+                    if len(tmp_line) > min_value and len(set(tmp_line)) > int(min_value//2):
                         corpus.append(tmp_line)
-    print(f"corpus length: {len(corpus)}")
+    logger.info(f"corpus length: {len(corpus)}")
     return corpus
 
 
 def get_dictionary(corpus: tp.List[tp.List[str]]):
     """
+    For each word in a corpus get id.
+    Return certain mapping and inverse mapping
 
-    :param corpus:
-    :return:
+    Args:
+        :param corpus: List of prepared list of words
+    :return: pair: mapping from a word to a word index, inverse mapping for the first one
     """
     flatten_corpus = list(itertools.chain.from_iterable(corpus))
     dictionary = {word: index
@@ -64,10 +76,15 @@ def get_dictionary(corpus: tp.List[tp.List[str]]):
 
 def get_word_probability_dict(corpus: tp.List[tp.List[str]]):
     """
+    For each word in a corpus get frequency of this word in the corpus.
+    Return certain mapping and inverse mapping
 
-    :param corpus:
-    :return:
+    Args:
+        :param corpus: List of prepared list of words
+    :return: pair: mapping from a word to a frequency of this word in the
+            corpus, inverse mapping for the first one
     """
+
     flatten_corpus = list(itertools.chain.from_iterable(corpus)) + ["<unk>"]
     tmp_freq_dict = dict(Counter(flatten_corpus))
     for k in tmp_freq_dict.keys():
@@ -80,11 +97,16 @@ def coocurance_martix(corpus: tp.List[tp.List[str]],
                       dictionary: tp.Dict[str, int],
                       window: int) -> np.ndarray:
     """
+    To speed up the learning process, we should create a matrix of the
+    occurrence of words in the context.
+    If the words met in the context of a certain size, then put 1
 
-    :param corpus:
-    :param dictionary:
-    :param window:
-    :return:
+    Args:
+        :param corpus: List of prepared list of words
+        :param dictionary: mapping from a word to a word index
+        :param window: what is a width of our context size
+    :return: matrix. Where mtx[i,j] == 1 if i and j were in the same context.
+            Otherwise 0
     """
     dict_lnegth = len(dictionary.keys())
     mtx = np.zeros(shape=(dict_lnegth, dict_lnegth))
@@ -106,13 +128,16 @@ def coocurance_martix(corpus: tp.List[tp.List[str]],
 
 def positive_sampling(line: tp.List[str],
                       window: int,
-                      count_in_line: int):
+                      count_in_line: int) -> tp.List[tp.Tuple[str, str]]:
     """
+    In a line, select several points (words) to define the input word.
+    Get pairs within the context of these words.
 
-    :param line:
-    :param window:
-    :param count_in_line:
-    :return:
+    Args:
+        :param line: list of prepared words
+        :param window: what is a width of our context size
+        :param count_in_line: how many samples should we get from one line in corpus
+    :return: List of pairs from the same context
     """
     line_length = len(line)
     rnd_ixs = np.random.choice(line_length - 1,
@@ -132,30 +157,36 @@ def positive_sampling(line: tp.List[str],
     return pairs
 
 
+def get_negative_pair(freq_dict: tp.Dict[str, float]):
+    """
+    get a pair of negative words.
+
+    Args:
+        :param freq_dict: mapping from a word to a frequency of this word in the corpus
+    :return: the pair of negative words based on statistics
+    """
+    return [
+        np.random.choice(list(freq_dict.keys()),
+                         p=list(freq_dict.values()))
+        for _ in range(2)]
+
+
 def negative_sampling(freq_dict: tp.Dict[str, float],
                       positive_pairs: tp.List[tp.Tuple[str, str]]):
     """
+    Randomly, based on statistics, get a list of pairs of words from the corpus
 
-    :param freq_dict:
-    :param positive_pairs:
-    :return:
+    Args:
+        :param freq_dict: mapping from a word to a frequency of this word in the corpus
+        :param positive_pairs: List of words' pairs that were in the same context
+    :return: List of words' pairs based on statistics
     """
     pairs = []
-
-    def get_negative_pair():
-        """
-
-        :return:
-        """
-        return [
-            np.random.choice(list(freq_dict.keys()),
-                             p=list(freq_dict.values()))
-            for _ in range(2)]
 
     pos_length = len(positive_pairs)
     neg_length = 0
     while neg_length < pos_length:
-        neg_pair = get_negative_pair()
+        neg_pair = get_negative_pair(freq_dict)
         if neg_pair not in positive_pairs and neg_pair[0] != neg_pair[1]:
             neg_length += 1
             pairs.append(neg_pair)
@@ -166,20 +197,11 @@ def negative_coocurance_sampling(freq_dict: tp.Dict[str, float],
                                  vocab: tp.Dict[str, int],
                                  N: int):
     """
-
-    :param freq_dict:
-    :param vocab:
-    :param N:
+    yeat another implementation for obtaining negative samples.
+    :param freq_dict: mapping from a word to a frequency of this word in the corpus
+    :param vocab: mapping from a word to a word index
+    :param N: size of these pairs
     :return:
     """
-    pairs = [
-        [
-            vocab[
-                np.random.choice(
-                    list(freq_dict.keys()), p=list(freq_dict.values())
-                )
-            ]
-            for _ in range(2)]
-        for _ in range(N)
-    ]
+    pairs = [[vocab[el] for el in get_negative_pair(freq_dict)] for _ in range(N)]
     return pairs
