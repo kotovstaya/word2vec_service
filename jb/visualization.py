1376f15bc47fc0a53f1fa5244ebb63803d64ac5a
Anton <asav@192.168.1.124>
2022-10-13 23:16:44 +0300

    [TEST-10] docstring for visualization.py added

--- jb/visualization.py
+++ jb/visualization.py
@@ -14,10 +14,13 @@ from preprocessing import get_dictionary, preprocess_corpus
 def get_word2vec_embeddings_as_array(corpus_path: str,
                                      model_path: str) -> np.array:
     """
+    Get Word2Vec model from gensim and get corpus of files.
+    Extract embeddings for every word in vocabulary in on 2D array
 
-    :param corpus_path:
-    :param model_path:
-    :return:
+    Args:
+        :param corpus_path: where is file with all documents
+        :param model_path: where is the trained model
+    :return: array with words' embeddings
     """
     model = Word2Vec.load(model_path)
 
@@ -42,8 +45,11 @@ def get_word2vec_embeddings_as_array(corpus_path: str,
 
 def get_custom_word2vec_embeddings_as_array(model_path: str) -> np.array:
     """
+    Get Word2Vec custom model from
+    Extract embeddings for every word in vocabulary in on 2D array
 
-    :param model_path:
+    Args:
+        :param model_path: where is the trained model
     :return:
     """
     model = torch.load(model_path)
@@ -55,32 +61,52 @@ def get_custom_word2vec_embeddings_as_array(model_path: str) -> np.array:
 def get_reducer_embeddings(reducer_class: tp.Any, embeddings: np.array,
                            n_components: int) -> np.array:
     """
+    For visualization purposes we should train a model that can help reduce
+    dimensions of our embeddings to a smaller space.
 
-    :param reducer_class:
-    :param embeddings:
-    :param n_components:
-    :return:
+    Args:
+        :param reducer_class: class instance for reduction
+        :param embeddings: array with embeddings in a N-dimension space
+        :param n_components: what a dimension size will take our embeddings after reduction
+    :return: trained model
     """
     reducer = reducer_class(n_components=n_components)
     return reducer.fit_transform(embeddings)
 
 
 def get_tsne_embeddings(embeddings: np.array, n_components: int) -> np.array:
+    """
+    implement get_reducer_embeddings function with TSNE model
+
+    Args:
+        :param embeddings: array with embeddings in a N-dimension space
+        :param n_components: what a dimension size will take our embeddings after reduction
+    :return: trained model
+    """
     return get_reducer_embeddings(TSNE, embeddings, n_components)
 
 
 def get_umap_embeddings(embeddings: np.array, n_components: int) -> np.array:
+    """
+    implement get_reducer_embeddings function with UMAP model
+
+    Args:
+        :param embeddings: array with embeddings in a N-dimension space
+        :param n_components: what a dimension size will take our embeddings after reduction
+    :return: trained model
+    """
     return get_reducer_embeddings(UMAP, embeddings, n_components)
 
 
 def plot_representation(embeddings: np.array, title: str,
                         filepath: str) -> None:
     """
+    Plot 2d embeddings
 
-    :param embeddings:
-    :param title:
-    :param filepath:
-    :return:
+    Args:
+        :param embeddings: array with 2d embeddings
+        :param title: string with name of the plot
+        :param filepath: where save this plot
     """
     plt.figure(figsize=(10, 5))
     plt.grid()
